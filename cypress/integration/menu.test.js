5807f2ce45b8af3f5b7eeb4161561a5e445d9e57
Daniel Mundra <daniel.mundra@civicactions.com>
2021-06-03 11:13:44 -0700

    Adjusted mobile menu tests.

--- cypress/integration/menu.test.js
+++ cypress/integration/menu.test.js
@@ -1,8 +1,19 @@
-describe('Menu', () => {
-  it('should be accessible', () => {
-    cy.visit('/about/');
-    cy.get('.usa-menu-btn').click();
-    cy.get('.usa-accordion__button').click();
-    cy.checkA11yWithMultipleViewPorts();
+describe('Menu on mobile', () => {
+  it('submenu should be accessible when active', () => {
+    cy.viewport('iphone-6+')
+    cy.visit('/about/')
+    cy.get('.usa-menu-btn').click()
+    cy.get('.usa-accordion__button').click()
+    cy.injectAxe()
+    cy.checkA11y()
+  });
+
+  it('submenu should be accessible when not active', () => {
+    cy.viewport('iphone-6+')
+    cy.visit('/calendar')
+    cy.get('.usa-menu-btn').click()
+    cy.get('.usa-accordion__button').click()
+    cy.injectAxe()
+    cy.checkA11y()
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,5 @@
 // ***********************************************
-// This example commands.js shows you how to
+// This commands.js shows you how to
 // create various custom commands and overwrite
 // existing commands.
 //
