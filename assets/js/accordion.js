6e644742c2a1bf447c2471e9dd4c8381c80d0e16
Daniel Mundra <daniel.mundra@civicactions.com>
2022-08-05 11:24:55 -0700

    Adjusted the logic of expand/collapse all button to consider when sections are already expanded. Added a test.

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -13,15 +13,27 @@ window.onload = function() {
     });
 }
 
-function expandingAccordions() {
+function expandingAccordions(e) {
     const accordionHeaders = document.querySelectorAll('.accordion-heading');
+    let isOpen = e.dataset.open;
+    if (isOpen === 'false') {
+        e.dataset.open = 'true';
+        e.textContent = 'Collapse All';
+    } else {
+        e.dataset.open = 'false';
+        e.textContent = 'Expand All';
+    }
 
     Array.prototype.forEach.call(accordionHeaders, accordionHeader => {
 
         let target = accordionHeader.parentElement.nextElementSibling;
-        let expanded = accordionHeader.getAttribute('aria-expanded') === 'true' || false;
-        accordionHeader.setAttribute('aria-expanded', !expanded);
-        target.hidden = expanded;
+        if (isOpen === 'false') {
+            accordionHeader.setAttribute('aria-expanded', 'true');
+            target.hidden = false;
+        } else {
+            accordionHeader.setAttribute('aria-expanded', 'false');
+            target.hidden = true;
+        }
 
     });
 
--- cypress/integration/accordion.test.js
+++ cypress/integration/accordion.test.js
@@ -1,7 +1,7 @@
 describe('Accordion', () => {
     it('all expanded sections should be accessible', () => {
         cy.visit('/roles/procurement')
-        cy.get('button').contains('Expand/Collapse All').click()
+        cy.get('button').contains('Expand All').click()
         cy.checkA11yWithMultipleViewPorts()
     })
 
@@ -10,4 +10,12 @@ describe('Accordion', () => {
         cy.get('.accordion-heading').contains('Playbook').click()
         cy.checkA11yWithMultipleViewPorts()
     })
-}) 
+
+    it("can all be expanded and collapsed", () => {
+        cy.visit("/roles/procurement")
+        cy.get('button').contains('Expand All').click()
+        cy.get('.accordion-body').should('not.have.attr', 'hidden')
+        cy.get('button').contains('Collapse All').click()
+        cy.get('.accordion-body').should('have.attr', 'hidden')
+    });
+})
