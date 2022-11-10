691dc127bcb3f8284c146e414ced34b9ea0bf771
Daniel Mundra <dmundra@users.noreply.github.com>
2022-11-10 09:17:18 -1000

    Alt text collector proof of concept (#664)
    * Using cypress to collect and log image alt texts.
    
    * Added x2js package and images.test.js that will collect images and alt text for the site.
    
    * Added comments.

---
+++ cypress/integration/images.test.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+describe('Images', () => {
+  it('alt text is tracked', () => {
+    const filename = 'cypress/results/accessibility.civicactions.com-images-and-alt-texts.csv'
+    cy.writeFile(filename, 'URL,SRC,ALT\n')
+    // Get pages from sitemap (see commands.js).
+    cy.getSitemapLocations().then(pages => {
+      pages.forEach(page => {
+        if (path.extname(page[0]) !== '.pdf') {
+          cy.visit(page[0])
+          cy.get('body', {log: false}).then($body => {
+            if ($body.find('img').length > 0) {
+              cy.url({log: false}).then(url => {
+                cy.get('img', {log: false}).each(($image, index, $list) => {
+                  const imageDetails = url + ',' + $image.prop('src') + ',' + $image.attr('alt') + '\n';
+                  cy.writeFile(filename, imageDetails, {flag: 'a+', log: false})
+                })
+              })
+            }
+          })
+        }
+      })
+    })
+  })
+})
